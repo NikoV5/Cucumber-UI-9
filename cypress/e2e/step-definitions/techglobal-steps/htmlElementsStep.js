0fdf1340f85e6967b016660415e3f8cd2d3195eb
NikoV5 <NikoVourtsis7@gmail.com>
2024-06-03 19:10:12 -0500

    Added updated files

---
+++ cypress/e2e/step-definitions/techglobal-steps/htmlElementsStep.js
@@ -0,0 +1,19 @@
+const { Then, When } = require("@badeball/cypress-cucumber-preprocessor");
+
+const TGHtmlElementsPage = require("../../../pages/TGHtmlElements")
+const tgHtmlElementsPage = new TGHtmlElementsPage();
+
+
+
+Then(/^user should see the "([^"]*)" page heading$/, (args1) => {
+  console.log(args1);
+  return true;
+});
+
+When(/^user clicks on the "([^"]*)" button$/, (label) => {
+  tgHtmlElementsPage.clickButtonByText(label);
+});
+
+Then(/^the text under it should be "([^"]*)"$/, (message) => {
+  tgHtmlElementsPage.getButtonMessage().should('have.text', message);
+});
